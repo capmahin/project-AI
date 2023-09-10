17f691b1c4ca55960b289bbdda697bb1b87dbf00
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-09-10 14:52:26 +0600

    add interval fucntion

--- clinet/script.js
+++ clinet/script.js
@@ -3,3 +3,11 @@ import user from './assets/user.svg';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element){
+  element.textContent = '';
+
+  loadInterval= setInterval(()=>{},300)
+}
