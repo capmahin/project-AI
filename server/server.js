3075758d2b8d79791cbc4069acd478fdbc1171ef
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-09-11 16:20:15 +0600

    all done

--- server/server.js
+++ server/server.js
@@ -38,6 +38,7 @@ app.post('/',async(req,res)=>{
             bot:response.data.choices[0].text
         })
     } catch (error) {
-        
+        console.error(error)
+    res.status(500).send(error || 'Something went wrong');
     }
 })
