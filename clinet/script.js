9986a102b891b9c3112061f7d5ba8bdc8d2a76b8
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-09-11 11:51:16 +0600

    return timestamp and hexadecimalString

--- clinet/script.js
+++ clinet/script.js
@@ -35,4 +35,6 @@ function generateUniqueId(){
   const timestamp = Date.now();
   const randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
 }
