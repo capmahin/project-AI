b8493aae283ae38da2efdc2f4b986d2099381e31
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-09-11 14:20:17 +0600

    steup backend

--- server/server.js
+++ server/server.js
@@ -10,3 +10,13 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req,res)=>{
+    res.status(200).send({
+        message:"Jarvis at ur Service Sir"
+    })
+})
